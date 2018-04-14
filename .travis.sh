a0146cd7f27dcc082cd614378d8d799656497ed8
probonopd <probonopd@users.noreply.github.com>
2018-04-14 17:14:40 +0200

    Upload to transfer.sh

--- .travis.sh
+++ .travis.sh
@@ -72,7 +72,7 @@ fi
 echo "Running tests"
 echo "Environment: $(uname -a)"
 
-sudo apt -y install libasound2-dev libopus-dev python-dev default-jdk swig # swig3.0
+sudo apt -y install libasound2-dev libopus-dev python-dev curl default-jdk swig # swig3.0
 # sudo ln -s /usr/bin/swig3.0 /usr/bin/swig
 ls /usr/bin/python*
 sudo rm /usr/bin/python3 || true
@@ -118,6 +118,9 @@ cd -
 tar cfvj ../pjsip-python-$VERSION-$ARCH.tar.bz2 pjsip-apps/src/python
 ls -lh ../pjsip-python-$VERSION-$ARCH.tar.bz2
 
+wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
+curl --upload-file ../pjsip-python-$VERSION-$ARCH.tar.bz2 https://transfer.sh/
+  
 exit 0
 
 #######
